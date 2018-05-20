87a12325b4f655b30a065b4aa96f51dbaca92e29
luccasoli <oliveira.mesquita@aluno.uece.br>
2018-05-20 18:56:34 -0300

    IPC com Pipes concluído

--- pipe/mp_pipe_1_para_1.py
+++ pipe/mp_pipe_1_para_1.py
@@ -1,21 +1,39 @@
-import multiprocessing, os
+import multiprocessing, os, random, time, threading
+
+def write(pipe):
+    r, w = pipe
+    number = random.randint(0,100) # Gera um número aleatório
+    w.send(number) # Envia o número para o Pipe
+    print("Mensagem escrita no Pipe: {}".format(number))
+    w.close() # Bloqueia escritas no Pipe
+
+def read(pipe):
+    r, w = pipe
+    while True:
+        try:
+            msg = r.recv()    # Lê da saída do Pipe
+            print("Thread {} recebe: {}".format(threading.get_ident(), msg))
+            
+        except EOFError:
+            break
+        except OSError:
+            print("OSError")
+            break
 
 def mp_pipe_1_para_1():
-    # Cria um Pipe
-    r, w = multiprocessing.Pipe()
+    r, w = multiprocessing.Pipe() # Cria um Pipe de comunicação
 
-    if(os.fork() == 0):
-        # Processo Pai
-        r.close() # Fecha a leitura no Pipe
-        msg = "Texto" 
-        print("Processo Pai envia: {}".format(msg))
-        w.send(msg) # Escreve no Pipe
+    writer = threading.Thread(target=write, args=((r, w), )) # Cria a Thread que escreve no Pipe
+    writer.start() # Inicia a Thread que escreve no Pipe
+    writer.join() # Aguarda a Thread terminar de escrever
 
-    else:
-        # Processo Filho
-        w.close() # Fecha a escrita no Pipe
-        msg = r.recv() # Lê do Pipe
-        print("Processo Filho recebe: {}\tE retorna o inverso: {}".format(msg, msg[::-1]))
+    start = time.time() # Inicia a contagem do tempo de processamento de leitura
+
+    reader = threading.Thread(target=read, args=((r, w), )) # Cria a Thread que lê do Pipe
+    reader.start() # Inicia a Thread que lê do Pipe
+    reader.join() # Aguarda a Thread terminar de ler
+
+    print("Tempo de leitura = {}".format(time.time() - start)) # Exibe a duração do tempo de leitura
 
 if __name__ == "__main__":
     mp_pipe_1_para_1()
--- pipe/mp_pipe_1_para_5.py
+++ pipe/mp_pipe_1_para_5.py
@@ -2,19 +2,18 @@ import multiprocessing, os, sys, time, random, threading
 
 def write(pipe):
     r, w = pipe
-    msg = "Texto"
     for j in range(5):
-        number = random.randint(0,100)
-        w.send(number)
+        number = random.randint(0,100) # Gera um número aleatório
+        w.send(number) # Envia o número para o Pipe
         print("Mensagem escrita no Pipe {}".format(number))
     w.close()
 
-def reader(pipe, t):
+def read(pipe, t):
     r, w = pipe
-    l, barrier = t
+    l, barrier = t # l = Lock e barrier = Barrier()
     while True:
         try:
-            barrier.wait() # Espera todas as threads chegarem nesse ponto
+            barrier.wait() # Espera todas as Threads chegarem nesse ponto
             l.acquire() # Trava região crítica
             
             msg = r.recv()    # Lê da saída do Pipe
@@ -29,7 +28,7 @@ def reader(pipe, t):
             l.release() # Destrava
 
 def mp_pipe_1_para_5():
-    barrier = threading.Barrier(5) # Entidade de sincronização das threads
+    barrier = threading.Barrier(5) # Entidade de sincronização das Threads
     r, w = multiprocessing.Pipe() # Cria um Pipe de comunicação
     writer = threading.Thread(target=write, args=((r, w), )) # A Thread que escreve no Pipe
 
@@ -37,19 +36,19 @@ def mp_pipe_1_para_5():
 
     l = multiprocessing.Lock() # Instancia uma trava
 
-    reader_p = list()
+    reader = []
 
     for i in range(5): # Cria uma lista de processos 
-        reader_p.append(threading.Thread(target=reader, args=((r, w), (l, barrier))))
+        reader.append(threading.Thread(target=read, args=((r, w), (l, barrier))))
 
-    start = time.time()
+    start = time.time() # Inicia a contagem do tempo de processamento de leitura
     for i in range(5):
-        reader_p[i].start() # Executa 5 processos que leem do Pipe
+        reader[i].start() # Executa 5 Threads que leem do Pipe
 
     for i in range(5):
-        reader_p[i].join() # Espera as 5 thread concluirem para continuar
+        reader[i].join() # Espera as 5 Threads concluirem para continuar
 
-    print("Tempo de leitura = {}".format(time.time() - start))
+    print("Tempo de leitura = {}".format(time.time() - start)) # Exibe a duração do tempo de leitura
 
 if __name__ == "__main__":
     mp_pipe_1_para_5()
