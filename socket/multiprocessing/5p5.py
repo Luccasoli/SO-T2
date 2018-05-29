4499888fe5cb79852e514e8b5fb5d085fea8b589
Gabriel Alves <sousa.alves@aluno.uece.br>
2018-05-29 10:50:26 -0300

    work done marromeno

--- socket/multiprocessing/5p5.py
+++ socket/multiprocessing/5p5.py
@@ -3,50 +3,43 @@ import multiprocessing as mp
 import os
 import random
 import time
-import queue
 
 
 ## Process Information
 
-QTD_SERVER_PROCS = 1
+QTD_SERVER_PROCS = 5
 QTD_SERVER_PROCS_COUNTER = 0
-QTD_CLIENT_PROCS = 1
+QTD_CLIENT_PROCS = 5
 QTD_CLIENT_PROCS_COUNTER = 0
-q = queue.Queue()
+
 
 ## Sockets Information
 
 host = '127.0.0.1'
-port = 2426
+port = 2427
 addr = (host, port)
 
 
 ## Sockets
-'''
-server_socket = mp.Queue()
-for i in range(QTD_SERVER_PROCS):
-    server_socket.put(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
-client_socket = mp.Queue()
-for i in range(QTD_CLIENT_PROCS):
-    client_socket.put(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
-'''
+
+server_procs = []
+client_procs = []
+
 
 ## Functions
 
 def rnum():
     return random.randint(1, 100)
 
-def enfileirando(q):
-    q.put(rnum())
-
 
 ## Starting Sockets
 
 def server_start():
+    time.sleep(1)
     global QTD_CLIENT_PROCS_COUNTER
     global QTD_SERVER_PROCS_COUNTER
-    #server = server_socket.get()
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print("Server-{} started".format(os.getpid()))
     server.bind(addr)
     server.listen(QTD_CLIENT_PROCS)
@@ -54,19 +47,20 @@ def server_start():
         connection, client_addr = server.accept()
         while(QTD_CLIENT_PROCS_COUNTER < QTD_CLIENT_PROCS):
             message_received = connection.recv(64)
-            #print("{} received".format(message_received))
             if not message_received:
                 break
             print("Server-{} received {}".format(os.getpid(), message_received))
             QTD_CLIENT_PROCS_COUNTER += 1
         print('teste1')
         QTD_SERVER_PROCS_COUNTER += 1
-        break
+        if(QTD_CLIENT_PROCS_COUNTER == QTD_CLIENT_PROCS):
+            QTD_CLIENT_PROCS_COUNTER = 0
+            break
     print('teste2')
     connection.close()
 
 def client_start(num):
-    time.sleep(0.2)
+    time.sleep(2)
     #client = client_socket.get()
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Client-{} started".format(os.getpid()))
@@ -76,14 +70,21 @@ def client_start(num):
     client.sendto(message.encode(), addr)
 
 
-## main
-
 #if __name__ == 'main':
-server_proc = mp.Process(target=server_start)
-server_proc.daemon = True
-client_proc = mp.Process(target=client_start, args=(rnum(), ))
-client_proc.daemon = True
-server_proc.start()
-#server_proc.join()
-client_proc.start()
-server_proc.join()
+
+
+for sp in range(QTD_SERVER_PROCS):
+    server_procs.append(mp.Process(target=server_start))
+    server_procs[sp].daemon = True
+
+for cp in range(QTD_CLIENT_PROCS*QTD_SERVER_PROCS):
+    client_procs.append(mp.Process(target=client_start, args=(rnum(), )))
+    client_procs[cp].daemon = True
+
+for i in range(QTD_SERVER_PROCS):
+    server_procs[i].start()
+    for j in range(QTD_CLIENT_PROCS):
+        client_procs[j].start()
+        client_procs[j].join()
+    del client_procs[0:5]
+    server_procs[i].join()
