70b81fc9714b1d708dad4e5cd9312de9c8bbc72e
luccasoli <oliveira.mesquita@aluno.uece.br>
2018-06-02 15:53:50 -0300

    update

--- shared-memory/comunicacao5p5.py
+++ shared-memory/comunicacao5p5.py
@@ -1,38 +1,45 @@
 import os
 from multiprocessing import Process, Value #importacoes para usar processos e memoria compartilhada
 import random
-from memory_profiler import profile
-
-myVar = [] #lista vazia
-
+'''
 def gera_num():
     return random.randint(1, 100)#funcao para gerar mensagem entre 1 e 100
 
+ 
+myVar= [] #lista vazia
+ 
+for i in range (5):
+    myVar.append(Value("i", gera_num()))#acrescentando na lista
 
 def funcaoFilho():
+   
     print (". (PID=" + str(os.getpid())+"). Mensagem para filho: "+ str(myVar[i].value)) #gera a mensagem aleatoria
-
-
 def funcaoLeitura():
     for i in range (5):
-        print (". (PID=" + str(os.getpid())+"). Mensagem do pai: "+ str(myVar[i].value))    #ler a mensagem
+   	 print (". (PID=" + str(os.getpid())+"). Mensagem do pai: "+ str(myVar[i].value))    #ler a mensagem
 
-
-@profile
-def main():
-    for i in range (5):
-        myVar.append(Value("i", gera_num()))#acrescentando na lista
+for i in range(5):
+    newP = Process(target=funcaoFilho) #criacao dos processos para chamar a funcaoFilho
+    newP.start()
+    newP.join()
     
-    for i in range(5):
-        newP = Process(target=funcaoFilho) #criacao dos processos para chamar a funcaoFilho
-        newP.start()
-        newP.join()
-        
-    for i in range(5):
-        newP = Process(target=funcaoLeitura) #criacao dos processo para chamar a funcaoleitura
-        newP.start()
-        newP.join()
+for i in range(5):
+    newP = Process(target=funcaoLeitura) #criacao dos processo para chamar a funcaoleitura
+    newP.start()
+    newP.join()
+'''
+
+def write():
+    pass
 
 
-if __name__ == "__main__":
-    main()
+def read():
+    pass
+
+
+def main():
+    writers = []
+    readers = []
+    shared_memories = []
+
+    for
